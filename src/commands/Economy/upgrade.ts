b71409ed62f40612b94e6d00cf85d95c4eab7fa3
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-06-28 20:35:47 +1000

    Upgrading

---
+++ src/commands/Economy/upgrade.ts
@@ -0,0 +1,44 @@
+import { ApplyOptions } from '@sapphire/decorators'
+import { ChatInputCommand, Command, RegisterBehavior } from '@sapphire/framework'
+import type { Message } from 'discord.js'
+
+@ApplyOptions<Command.Options>({
+  description: 'Power-up!',
+})
+export class Upgrade extends Command {
+  public override registerApplicationCommands(registry: ChatInputCommand.Registry): void {
+    registry.registerChatInputCommand(
+      (builder) =>
+        builder
+          .setName(this.name)
+          .setDescription(this.description),
+      {
+        behaviorWhenNotIdentical: RegisterBehavior.Overwrite,
+      }
+    )
+  }
+  
+  public override async chatInputRun(interaction: Command.ChatInputInteraction): Promise<Message | unknown> {
+    const id = interaction.user.id
+    const userData = await this.container.prisma.user.findUnique({ where: { id } })
+    const bits = userData!.bits - 150
+
+    if (bits < 0) {
+      return await interaction.reply('You need more bits to upgrade.')
+    }
+
+    const { level } = await this.container.prisma.user.update({
+      where: {
+        id
+      },
+      data: {
+        bits,
+        level: {
+          increment: 1
+        }
+      }
+    })
+
+    return await interaction.reply(`You have leveled up to LVL ${level}!`)
+  }
+}
